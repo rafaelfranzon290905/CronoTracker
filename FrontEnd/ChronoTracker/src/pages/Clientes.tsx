b967caf13621064563d83dfdfee72718b22322b7
Bianca Franzon <biancafranzon2909@gmail.com>
2025-10-03 03:15:06 -0300

    Implementação de rascunho de Dashboard e Rotas com React Router

---
+++ FrontEnd/ChronoTracker/src/pages/Clientes.tsx
@@ -0,0 +1,10 @@
+
+function Clientes() {
+  return (
+    <main className="py-10">
+      <h1 className="bg-yellow-500">Início da página clientes</h1>
+    </main>
+  )
+}
+
+export default Clientes

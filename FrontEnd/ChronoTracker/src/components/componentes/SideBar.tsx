a7eafaf3adc1c1f5f7ab3e6beec4239c44a9fb2f
RafaBrietzke <rlbrietzke@gmail.com>
2025-10-07 20:29:03 -0300

    feature: incluir colaboradores no sidebar

--- FrontEnd/ChronoTracker/src/components/componentes/SideBar.tsx
+++ FrontEnd/ChronoTracker/src/components/componentes/SideBar.tsx
@@ -18,6 +18,7 @@ export default function SideBar() {
           <Button variant="ghost" className="w-full justify-start bg-blue-900 text-white"><Home className="mr-2 h-4 w-4" /> Dashboard</Button>
           <Link to="/TimeSheet"><Button variant="ghost" className="w-full justify-start"><Clock className="mr-2 h-4 w-4" /> TimeSheet</Button></Link>
           <Link to="/clientes"><Button variant="ghost" className="w-full justify-start"><Users className="mr-2 h-4 w-4" /> Clientes</Button></Link>
+          <Link to="/collaborators"><Button variant="ghost" className="w-full justify-start"><Users className="mr-2 h-4 w-4" /> Colaboradores</Button></Link>
           <Link to="/projetos"><Button variant="ghost" className="w-full justify-start"><Rocket className="mr-2 h-4 w-4" /> Projetos</Button></Link>
           <Button variant="ghost" className="w-full justify-start"><Activity className="mr-2 h-4 w-4" /> Atividades</Button>
           <Button variant="ghost" className="w-full justify-start"><FileText className="mr-2 h-4 w-4" /> Relatórios</Button>

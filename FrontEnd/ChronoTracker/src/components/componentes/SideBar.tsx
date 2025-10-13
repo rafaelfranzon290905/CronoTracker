511e66e106ce1df0a2949057541192df8a23cad0
Bianca Franzon <biancafranzon2909@gmail.com>
2025-10-13 18:32:38 -0300

    Adicionado a componentização e responsividade da header e do SideBar

--- FrontEnd/ChronoTracker/src/components/componentes/SideBar.tsx
+++ FrontEnd/ChronoTracker/src/components/componentes/SideBar.tsx
@@ -1,22 +1,20 @@
 import { Link } from "react-router-dom"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts"
 import { Home, Users, FileText, DollarSign, Clock, Rocket, Activity } from "lucide-react"
-import CronosAzul from "../../imagens/ChronosAzul.png"
+import ChronosAzulFundoRemovido from "../../imagens/ChronosAzulFundoRemovido.png"
 
 export default function SideBar() {
     return (
         <>
         {/* Sidebar */}
-      <aside className="w-64 bg-blue-50 shadow-2xl flex flex-col rounded-4xl m-4">
+      <aside className="w-64 bg-blue-50 shadow-2xl flex flex-col rounded-4xl m-4 hidden md:block">
         <div className="p-6 flex items-center space-x-2 font-bold text-lg">
-          <img src={CronosAzul} className="h-12 w-12" />
+          <img src={ChronosAzulFundoRemovido} className="h-12 w-12" />
           <span>CHRONO TRACKER</span>
         </div>
         <nav className="flex-1 px-4 space-y-2 text-sm">
           <Button variant="ghost" className="w-full justify-start bg-blue-900 text-white"><Home className="mr-2 h-4 w-4" /> Dashboard</Button>
-          <Link to="/TimeSheet"><Button variant="ghost" className="w-full justify-start"><Clock className="mr-2 h-4 w-4" /> TimeSheet</Button></Link>
+          <Link to="/TimeSheet"><Button variant="ghost" className="w-full justify-start"><Clock className="mr-2 h-6 w-6" /> TimeSheet</Button></Link>
           <Link to="/clientes"><Button variant="ghost" className="w-full justify-start"><Users className="mr-2 h-4 w-4" /> Clientes</Button></Link>
           <Link to="/collaborators"><Button variant="ghost" className="w-full justify-start"><Users className="mr-2 h-4 w-4" /> Colaboradores</Button></Link>
           <Link to="/projetos"><Button variant="ghost" className="w-full justify-start"><Rocket className="mr-2 h-4 w-4" /> Projetos</Button></Link>
--- FrontEnd/ChronoTracker/src/pages/Timesheets/TimeSheetPlanilha.tsx
+++ FrontEnd/ChronoTracker/src/pages/Timesheets/TimeSheetPlanilha.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
+import Header from "@/components/componentes/Header"
+import SideBar from "@/components/componentes/SideBar"
 
 export default function TimeSheetPlanilha(){
     return(
         <>
-            <main className="py-10">
-            <h1 className="bg-yellow-500">Início da página TimeSheet</h1>
-    </main>
+            <div className="flex h-screen bg-gray-50">
+      
+                <SideBar/>
+
+
+        {/* Conteúdo */}
+        <main className="flex-1 p-6 overflow-auto">
+            {/* Header com Searchbar */}
+            <Header/>
+            <h1 className="bg-yellow-500">Início da página TimeSheets</h1>
+            </main>
+            </div>
         </>
     )
 }

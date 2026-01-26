ac505a3e33b0f0b6c7f409510cf1e463d6a0515f
RafaBrietzke <rlbrietzke@gmail.com>
2026-01-26 13:07:47 -0300

    feat: cria tela de controle de projeto

--- FrontEnd/ChronoTracker/src/lib/projects.ts
+++ FrontEnd/ChronoTracker/src/lib/projects.ts
@@ -1,23 +1,49 @@
-export type Projeto = {
-  projeto_id: number;
-  nome_projeto: string;
-  cliente_id: number;
-  clientes?: { 
-    nome_cliente: string;
-  };
-  descricao: string;
-  data_inicio: string; 
-  data_fim: string;
-  status: boolean;
-  atividades?: {
-    atividade_id: number;
-    nome_atividade: string;
+// export type Projeto = {
+//   projeto_id: number;
+//   nome_projeto: string;
+//   cliente_id: number;
+//   clientes?: { 
+//     nome_cliente: string;
+//   };
+//   descricao: string;
+//   data_inicio: string; 
+//   data_fim: string;
+//   status: boolean;
+//   atividades?: {
+//     atividade_id: number;
+//     nome_atividade: string;
+//     status: boolean;
+//   }[];
+//   projeto_colaboradores?: {
+//       colaboradores: {
+//         colaborador_id: number;
+//         nome_colaborador: string;
+//       }
+//     }[];
+// };
+
+export interface Projeto {
+    projeto_id: number;
+    cliente_id: number;
+    nome_projeto: string;
+    descricao?: string;
+    data_inicio: string;
+    data_fim: string;
     status: boolean;
-  }[];
-  projeto_colaboradores?: {
+    horas_previstas: number; // <-- Novo campo
+    horas_consumidas?: number;
+    clientes?: {
+        nome_cliente: string;
+    };
+    atividades?: {
+      atividade_id: number;
+        nome_atividade: string;
+        status: boolean;
+    }[];
+    projeto_colaboradores?: {
       colaboradores: {
         colaborador_id: number;
         nome_colaborador: string;
       }
     }[];
-};
+}
